295fd43d5c28351980e48018a8b5216e181c0f7f
Jane <janneee24@gmail.com>
2019-11-06 14:39:42 +0300

    Updates

---
+++ precache-manifest.6e17762c645712881a4c5f368e951660.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd98aafeffa4cd8b477c5c3a038f4c5e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "403660f217dd732277b4",
+    "url": "./static/css/2.70f3b80d.chunk.css"
+  },
+  {
+    "revision": "403660f217dd732277b4",
+    "url": "./static/js/2.7e0eaf3a.chunk.js"
+  },
+  {
+    "revision": "9cae7a03535e789160eb",
+    "url": "./static/js/main.834ce8f9.chunk.js"
+  },
+  {
+    "revision": "029bad73ea0834bf743b",
+    "url": "./static/js/runtime-main.c006d456.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.05406652123868ba0dff93ffb3636c50.js"
+  "./precache-manifest.6e17762c645712881a4c5f368e951660.js"
 );
 
 self.addEventListener('message', (event) => {
