08bf603941aa768e19879def02ad03738cba7bb7
Jane <janneee24@gmail.com>
2019-11-22 15:51:18 +0300

    Update

---
+++ src/components/Tasks.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import {List, Cell, PanelHeader, platform, ANDROID, Header, Div, Group, Gallery, Button} from '@vkontakte/vkui'
+import '@vkontakte/vkui/dist/vkui.css'
+
+
+class Tasks extends React.Component {
+	render() {
+
+        let {
+            tasks,
+            router,
+            setCurrentTaskId,
+            deleteTask,
+            removable
+        } = this.props
+
+        const osname = platform()
+
+		return (
+			<div>
+                <PanelHeader
+
+                >
+                    Тесты
+                </PanelHeader>
+                <List style={{ paddingTop : (osname === ANDROID) ? 56 : 48 }}>
+                    {
+                        tasks.map((task, index) => (
+                            <Cell
+                                multiline
+                                expandable
+                                removable={removable}
+                                key={index}
+                                onRemove={() => deleteTask(task.id)}
+
+                            >
+                                <Header><b>{task.name}</b></Header>
+                                <Gallery
+                                    slideWidth="90%"
+                                    style={{ height: 150 }}
+                                >
+                                    <img src={task.image_back}/>
+                                </Gallery>
+                                <Div>{task.text}</Div>
+                                <div align="center"><Button onClick={()=> {
+                                    setCurrentTaskId(task.id)
+                                    router.navigate('task', { id : task.id })
+                                }
+                                }>Начать тест</Button></div>
+
+                            </Cell>
+                        ))
+                    }
+                </List>
+            </div>
+		);
+	}
+}
+
+export default Tasks;
